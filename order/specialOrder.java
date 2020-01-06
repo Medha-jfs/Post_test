0adcb10da4148196e9a3529c9e8be460f73c5e3f
Medha-jfs <59194616+Medha-jfs@users.noreply.github.com>
2020-01-05 19:53:18 -0800

    Add files via upload

---
+++ order/specialOrder.java
@@ -0,0 +1,11 @@
+package order;
+
+public abstract class specialOrder implements normalOrder {
+	public abstract void confirm();
+	public abstract void close();
+	public abstract void dispatch();
+	
+	
+	
+
+}
